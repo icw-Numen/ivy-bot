bb33f2f2b33eb34cab4d48285d91793a16aef173
icw-Numen <40610172+icw-Numen@users.noreply.github.com>
2018-07-14 23:28:09 -0300

    Added base files
    Added files from the bot's "root" directory

---
+++ index.js
@@ -0,0 +1,96 @@
+//COPY ALL NECESSARY CODE
+
+
+
+// DEPENDENCIES
+
+// to install Discord.js, type in cmd "npm i discord.js --save"
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//const settings = require('./settings.json');
+
+// to install chalk, type in cmd "npm i chalk --save".
+// basically, chalk lets you recolor stuff in console to
+// differenciate debug from warnings to errors.
+const chalk = require('chalk');
+
+const fs = require('fs');
+
+const moment = require('moment');
+
+const sql = require('sqlite');
+sql.open('./scores.sqlite');
+sql.open('./guildsettings.sqlite');
+//sql.open('./serverimages.sqlite');
+
+exports.talkedRecently = new Set();
+
+//for music queue
+exports.servers = {};
+
+//for game sessions (WIP)
+exports.serversR = {};
+
+require('./util/eventLoader')(client);
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.cmdsByType = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+fs.readdir('./commands/', (err, files) => {
+  if (err) console.error(err);
+  log(`Loading a total of ${files.length} commands.`);
+  files.forEach(f => {
+    const props = require(`./commands/${f}`);
+    log(`Loading Command: ${props.help.name}. 👌`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./commands/${command}`)];
+      const cmd = require(`./commands/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  /* This function should resolve to an ELEVATION level which
+     is then sent to the command handler for verification*/
+  let permlvl = 0;
+  if (message.guild.member(client.user).hasPermission(['BAN_MEMBERS', 'KICK_MEMBERS'])) permlvl = 2;
+  if (message.guild.member(client.user).hasPermission('ADMINISTRATOR')) permlvl = 3;
+  if (message.author.id === process.env.OWNERID) permlvl = 4;
+  return permlvl;
+};
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e));
+});
+
+client.login(process.env.TOKEN);

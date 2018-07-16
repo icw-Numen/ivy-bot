ed7b8b8e1210dbf47ca4be1cc0b962fde2514223
Igor Chen Wu <i2wu@ucsd.edu>
2018-07-16 04:06:02 -0300

    updated unban and some other stuff

--- commands/unban.js
+++ commands/unban.js
@@ -1,57 +1,61 @@
 const {RichEmbed} = require('discord.js');
 const {caseNumber} = require('../util/caseNumber.js');
 const settings = require('../settings.json');
-
+const main = require('../app.js');
 const reactions = require('../reactions.json');
 
 exports.run = (client, message, args) => {
-  const server = message.guild;
-
-  sql.get(`SELECT * FROM channels WHERE guildId ="${server.id}"`).then(row => {
-    if (!row) {
-      sql.run('INSERT INTO channels (guildId, welcome, goodbye, modlog, autorole, muted) VALUES (?, ?, ?, ?, ?, ?)', [server.id, '', '', '', '', '']);
-      message.channel.send(`Please set a log channel with "${settings.prefix}modlog <channel>" first, ${message.author.username}`).catch(console.error);
+  const guild = message.guild;
+  main.guildsettings.findOne({ guildId : { $gte: guild.id }}, function (err, res) {
+    var row = res;
+    if (err) return console.log(err);
+    if (row) {
+      unHammer(row, message, args, guild, client);
     } else {
-      //separates reason from the message
-      const reason = args.slice(1).join(' ');
-
-      if (reason.length < 1) return message.channel.send(`Please give me a reason so I can unban them, ${message.author.username}`).catch(console.error);
-
-      const user = args[0];
-
-      const modlog = server.channels.find('name', row.modlog);
-
-      if (!modlog) return message.channel.send(`Please set a log channel with "${settings.prefix}modlog <channel>" first, ${message.author.username}`).catch(console.error);
-
-      if (!user) return message.channel.send(`Please give me a user ID (not user name or tag) so I can unban them for you, ${message.author.username}`).catch(console.error);
-
-      caseNumber(client, modlog).then(num => {
-        const reason = args.splice(1, args.length).join(' ') || `No reasons given (use ${settings.prefix}reason <case number> <reason> to set a reason for this action)`;
-        message.guild.unban(user);
-        let embed = new RichEmbed()
-          .setColor(0xF18E8E)
-          .setTitle('Unban Successful~')
-          .setThumbnail(reactions.smug2)
-          .setDescription(`The user with the ID of **${user}** has been unbanned. For more details, head over to #${modlog.name}`);
-        message.channel.send({embed});
-
-        embed = new RichEmbed()
-          .setColor(0xFFAD56)
-          .setTimestamp()
-          .setDescription(`**Action:** Unban\n**Target:** User ID: ${user}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
-          .setFooter(`Case ${num}`);
-        return client.channels.get(modlog.id).send({embed});
+      main.guildsettings.insertOne({ guildId: guild.id, welcome: '', goodbye: '', modlog: '', autorole: '' }, function (error) {
+        if (error) return console.log(err);
+        unHammer(row, message, args, guild, client);
+        return;
       });
     }
-  }).catch(() => {
-    console.error;
-    sql.run('CREATE TABLE IF NOT EXISTS channels (guildId TEXT, welcome TEXT, goodbye TEXT, modlog TEXT, autorole TEXT, muted TEXT)').then(() => {
-      sql.run('INSERT INTO channels (guildId, welcome, goodbye, modlog, autorole, muted) VALUES (?, ?, ?, ?, ?, ?)', [server.id, '', '', '', '', '']);
-    });
   });
 };
 
 
+// Helper method
+function unHammer(row, message, args, guild, client) {
+  //separates reason from the message
+  const reason = args.slice(1).join(' ');
+
+  if (reason.length < 1) return message.channel.send(`Please give me a reason so I can unban them, ${message.author.username}`).catch(console.error);
+
+  const user = args[0];
+
+  const modlog = guild.channels.find('name', row['modlog']);
+
+  if (!user) return message.channel.send(`Please give me a user ID (not user name or tag) so I can unban them for you, ${message.author.username}`).catch(console.error);
+
+  caseNumber(client, modlog).then(num => {
+    const reason = args.splice(1, args.length).join(' ') || 'No reason';
+    message.guild.unban(user);
+    let embed = new RichEmbed()
+      .setColor(0xF18E8E)
+      .setTitle('Unban Successful~')
+      .setThumbnail(reactions.smug2)
+      .setDescription(`The user with the ID of **${user}** has been unbanned. For more details, head over to #${modlog.name}`);
+    message.channel.send({embed});
+
+    embed = new RichEmbed()
+      .setColor(0xFFAD56)
+      .setTimestamp()
+      .setDescription(`**Action:** Unban\n**Target:** User ID: ${user}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}\n\n(Tip: Use \`${settings.prefix}reason <case number> <reason>\` to set a reason for this action)`)
+      .setFooter(`Case ${num}`);
+    return client.channels.get(modlog.id).send({embed});
+  });
+}
+
+
+// Command metadata
 exports.conf = {
   enabled: true,
   guildOnly: false,

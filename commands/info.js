efa573615c6c84522358e776dddb6f5730fe8ca8
Igor Chen Wu <i2wu@ucsd.edu>
2018-07-16 08:01:13 -0300

    changes to info

--- commands/info.js
+++ commands/info.js
@@ -19,7 +19,7 @@ exports.run = async (client, message) => {
     .addField('Deployed on:', 'Heroku', true)
     .addField('# of servers joined:', `${client.guilds.array().length}`, true)
     .addField('Three sizes:', 'B: [Christian manga], W: [redacted],\nH: [censored]', true)
-    .addField('Github:', 'https://github.com/icw-Numen/ivy-bot')
+    .addField('Github/Website/Wiki:', 'https://github.com/icw-Numen/ivy-bot')
     .addField('Ivy icons/art:', 'https://sta.sh/21qy8xqbpkxm')
     .setFooter(`Bot and icons/art made with care by ${package.author}`);
   return message.channel.send({embed});

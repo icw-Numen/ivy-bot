3b27271377149509d50483d91ac88ef5ccc9e610
icw-Numen <i2wu@ucsd.edu>
2018-07-18 16:29:11 -0300

    ayy

--- commands/play.js
+++ commands/play.js
@@ -36,13 +36,17 @@ exports.run = async (client, message) => {
       .setThumbnail(reactions.wink1)
       .setDescription(`Now playing music, ${user.username}`);
     message.channel.send({embed}).then(m => {
-      if (bot.hasPermission('MANAGE_MESSAGES')) {
-        setTimeout(function() {
-          m.delete();
-        }, 4000);
+      playHelper(server.vc, message);
+      if (server.dispatcher) {
+        if (!server.dispatcher.paused) {
+          if (bot.hasPermission('MANAGE_MESSAGES')) {
+            setTimeout(function() {
+              m.delete();
+            }, 3000);
+          }
+        }
       }
     });
-    playHelper(server.vc, message);
     return;
   }
 };

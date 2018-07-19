3d1a65e5bb0be67343fa6be3ee36687dccecbc6a
icw-Numen <i2wu@ucsd.edu>
2018-07-18 22:56:02 -0300

    welcome changes

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -8,7 +8,7 @@ module.exports = (guild) => {
     .setColor(0xF18E8E)
     .setThumbnail(reactions.normal2)
     .setTitle('Hello everyone~')
-    .setDescription(`~~Demon~~ Maid Ivy here, and thanks for ~~summoning~~ inviting me :9\n\nFor details of what I can do, please type \`${settings.prefix}botinfo\` or \`${settings.prefix}help\` for commands. \`${settings.prefix2}help\` and \`@Ivy help\` are fine too if you prefer calling me by my name~\n\nI\'ll do my best to serve all of you~`)
+    .setDescription(`~~Demon~~ Maid Ivy here, and thanks for ~~summoning~~ inviting me :9\n\nFor details of what I can do, please type \`${settings.prefix}info\` or type \`${settings.prefix}help\` for commands.\n\`${settings.prefix2}help\` and \`@Ivy help\` are fine too if you prefer calling me by my name~\n\nI\'ll do my best to serve all of you~`)
     .setFooter(`Bot and icons/art made with care by ${package.author}`);
   console.log(`Guild added: ${guild.name} (${guild.id}) has added me`);
   return guild.channels.get(guild.channels.find('name', 'general').id).send({embed});

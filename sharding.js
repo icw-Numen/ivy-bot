4151bb6624d369183ede3556dfa79bb273bb0cab
icw-Numen <i2wu@ucsd.edu>
2018-07-20 18:45:25 -0300

    ...

--- sharding.js
+++ sharding.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js');
 const Manager = new ShardingManager('./app.js');
-exports.manager = Manager;
+//exports.manager = Manager;
 Manager.spawn(2);
 
 Manager.on('launch', shard => console.log(`Shard ${shard.id}/${shard.manager.totalShards} loaded`));

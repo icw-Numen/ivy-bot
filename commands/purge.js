26de08ba930d40184591e948c5229002710538b3
icw-Numen <i2wu@ucsd.edu>
2018-07-20 03:30:46 -0300

    ...

--- commands/purge.js
+++ commands/purge.js
@@ -30,7 +30,7 @@ exports.run = (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['obliterate', 'delete', 'deletemsg', 'burnwithfire'],
+  aliases: ['obliterate', 'delete', 'deletemsg', 'burnwithfire', 'burn'],
 
   //this means only mods (permLevel 2) or higher can use this command
   permLevel: 2

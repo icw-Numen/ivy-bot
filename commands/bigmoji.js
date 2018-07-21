ff0a0059dc39f80dd4d72dbb29409920e0f5ceef
icw-Numen <i2wu@ucsd.edu>
2018-07-21 04:07:25 -0300

    ...

--- commands/bigmoji.js
+++ commands/bigmoji.js
@@ -14,7 +14,8 @@ exports.run = (client, message, args) => {
     extn = '.png';
   }
 
-  var regex = /\ud83d[\ude00-\ude4f]/g;
+  var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*/;
+
   let mojiid;
 
   if (args[0].match(regex)) {

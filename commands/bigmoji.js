2238877c2b491c8c8c608023e63f312eb8fee49c
icw-Numen <i2wu@ucsd.edu>
2018-07-21 02:56:24 -0300

    ...

--- commands/bigmoji.js
+++ commands/bigmoji.js
@@ -1,8 +1,16 @@
 const {RichEmbed} = require('discord.js');
 
 exports.run = (client, message, args) => {
-  if (args.length == 0 || (!/^<:/.test(args[0]) && !/$>/.test(args[0]))) {
+  if (args.length === 0) {
     return message.channel.send(`Please type a valid emoji, ${message.author.username}`).catch(console.error);
+  }
+
+  let extn;
+
+  if (/^<a:/.test(args[0])) {
+    extn = '.gif';
+  } else {
+    extn = '.png';
   }
 
   // removes all non-digits from args
@@ -15,7 +23,7 @@ exports.run = (client, message, args) => {
   const embed = new RichEmbed()
       .setColor(0x36393E)
       .setAuthor(`${message.author.username}`, `${message.author.avatarURL}`)
-      .setImage(`https://discordapp.com/api/emojis/${moji}`);
+      .setImage(`https://discordapp.com/api/emojis/${moji}${extn}`);
   message.channel.send({embed}).catch(error => {message.channel.send(`Please type a valid emoji, ${message.author.username}`).catch(error);});
 };
 

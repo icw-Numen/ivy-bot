9be06b5a1c530a5c014fbe5243eb5d423dfa685a
icw-Numen <i2wu@ucsd.edu>
2018-07-23 04:46:20 -0300

    fix danbooru bugs

--- commands/danbooru.js
+++ commands/danbooru.js
@@ -46,7 +46,7 @@ exports.run = (client, message, args) => {
 
     if (!post) {
       booru.posts({ tags: tag1 + tag2 + ' rating:explicit'}).then(loods => {
-        const index = Math.floor(Math.random() * posts.length);
+        const index = Math.floor(Math.random() * loods.length);
         const lood = loods[index];
         if (!lood) {
           return message.channel.send(`Oops, no lewds to be found here, ${message.author.username}~`).catch(console.error);
